4f929ada0d71b6f763727a758ab048eee6eb9797
Pearl Lam <wplam@email.arizona.edu>
2017-09-21 13:16:18 -0700

    adding unix-utils homework

--- problems/hello/hello-arg.sh
+++ problems/hello/hello-arg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
-    echo Usage: $0 name
+    echo Usage: $0 YOU ARE WRONG. THE NUMBER OF ARGUMENTS DOES NOT EQUAL TO 1
     exit 1
 fi
 
--- problems/hello/hello-arg2.sh
+++ problems/hello/hello-arg2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
-    echo Usage: $0 greeting name
+    echo Usage: $0 You need two arguments
     exit 1
 fi
 
