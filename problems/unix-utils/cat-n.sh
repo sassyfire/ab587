5e9fc2302f85502f2017e44d0d961a5a2086f7a6
Pearl Lam <wplam@email.arizona.edu>
2017-09-27 11:02:01 -0700

    almost there!

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-    printf "Usage: %s file\n" "$(basename "$0")"
-    exit 1
-fi
-
 FILE=$1
 i=0
 
-if [[ -f $FILE ]]; then
-   while read -r LINE; do
+if [[ $# -eq 0 ]]; then
+   printf "Usage: %s file\n" "$(basename "$0")"
+   exit 1
+
+else
+
+if [[ ! -f $FILE ]]; then
+   echo "$FILE is not a file"
+   exit 1
+fi
+fi
+
+while read -r LINE; do
    let i++
    echo "$i $LINE"
    done < $FILE
-else
-   echo "$FILE is not a file"
-fi
 
 
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -3,21 +3,22 @@
 if [[ $# -eq 0 ]]; then
     printf "Usage: %s [NUM] FILE\n" "$(basename "$0")" 
     exit 1
-
-fi
+fi 
 
 FILE=$1
 NUM=${2:-3}
 i=-1
 
-if [[ -f $FILE ]]; then
-    while read -r LINE; do
+if [[ ! -f $FILE ]]; then
+        echo "$FILE is not a file"
+        exit 1
+fi
+
+while read -r LINE; do
     let i++
     if [[ $i -eq $NUM ]]; then 
     break
-fi
-    echo "$LINE"
-done < $FILE
-else 
-    echo "$FILE is not a file"
-fi
+    fi
+        echo "$LINE"
+    done < $FILE
+
