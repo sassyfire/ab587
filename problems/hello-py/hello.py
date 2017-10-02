0c00be979caccda299fab2e2d9974d5a78e016ba
Pearl Lam <wplam@email.arizona.edu>
2017-10-02 11:13:50 -0700

    still working

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 """say hello to the arguments"""
 
-print('OK')
+
+import sys
+import os
+
+def main():
+    """main"""
+    args = sys.argv
+
+    if len(args) < 1:
+        script = os.path.basename(args[0])
+        print('Usage: {} NAME [NAME2 ...]'.format(script))
+        sys.exit(1)
+
+    print('Hello to the {}'.format(' '.str(args[1:])) + 'of you: {}!'.format(' , , and '.join(args[1:])))
+
+if __name__ == '__main__':
+    main()
