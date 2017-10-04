fb80e1d4eea91cfd32eb91ac488b98432985b3b6
Pearl Lam <wplam@email.arizona.edu>
2017-10-04 14:38:48 -0700

    working

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -5,16 +5,14 @@
 import sys
 import os
 
-def main():
-    """main"""
-    args = sys.argv
+args = sys.argv
 
-    if len(args) < 1:
-        script = os.path.basename(args[0])
-        print('Usage: {} NAME [NAME2 ...]'.format(script))
-        sys.exit(1)
+if len(args) < 1:
+    script = os.path.basename(args[0])
+    print('Usage: {} NAME [NAME2 ...]'.format(script))
+    sys.exit(1)
 
-    print('Hello to the {}'.format(' '.str(args[1:])) + 'of you: {}!'.format(' , , and '.join(args[1:])))
+name = args[1]
 
-if __name__ == '__main__':
-    main()
+if len(args) 
+    print('Hello to the {}'.format(len(args[1:])) + ' of you: {}!'.format(', '.join(args[1:])))
