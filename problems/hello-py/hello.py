5f406ccaa2e872351b04e9595bd03b7992f1c7a6
Pearl Lam <wplam@email.arizona.edu>
2017-10-05 09:20:44 -0700

    qc report 1

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -5,14 +5,18 @@
 import sys
 import os
 
-args = sys.argv
+args = sys.argv[1:]
 
 if len(args) < 1:
-    script = os.path.basename(args[0])
+    script = os.path.basename(sys.argv[0])
     print('Usage: {} NAME [NAME2 ...]'.format(script))
     sys.exit(1)
 
-name = args[1]
+name = args[0]
 
-if len(args) 
-    print('Hello to the {}'.format(len(args[1:])) + ' of you: {}!'.format(', '.join(args[1:])))
+if len(args) <= 2:
+    print('Hello to the {}'.format(len(args)) + ' of you: {}!'.format(' and '.join(args)))
+else:
+    print('Hello to the {}'.format(len(args)) + ' of you: {}'.format(', '.join(args[:-1])) + ', and {}!'.format(args[-1]))
+
+
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
+"""count your vowels"""
 
-print('OK')
+
+import sys
+import os
+
+args = sys.argv[1:]
+
+if len(args) < 1:
+    script = os.path.basename(sys.argv[0])
+    print('Usage: {} NAME STRING'.format(script))
+    sys.exit(1)
+
+name = args[0]
+
+vowels = "a", "e", "i", "o", "u"
+
+count = 0
+if vowels in args <= 1:
+   count = count + 1
+
+print('There is 1 vowel in "{}."'.format(args))
+
+count = 0
+if vowels in args !=1:
+   count = count + 1
+
+print('There are {} vowels '.format(len(vowels)) + 'in "{}."'.format(args))
