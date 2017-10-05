4e910370d9440ed422d1dcfcf7ece5ee82e876bc
Pearl Lam <wplam@email.arizona.edu>
2017-10-05 10:41:47 -0700

    i finished

--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -12,18 +12,46 @@ if len(args) < 1:
     print('Usage: {} NAME STRING'.format(script))
     sys.exit(1)
 
-name = args[0]
+word = args[0]
 
-vowels = "a", "e", "i", "o", "u"
-
-count = 0
-if vowels in args <= 1:
-   count = count + 1
-
-print('There is 1 vowel in "{}."'.format(args))
+vowels = "aeiou"
 
 count = 0
-if vowels in args !=1:
-   count = count + 1
+for char in word:
+    if char in vowels:
+        count = count + 1
 
-print('There are {} vowels '.format(len(vowels)) + 'in "{}."'.format(args))
+if count == 0:
+    print('There are 0 vowels in "{}."'.format(word))
+ 
+elif count == 1:
+
+    print('There is 1 vowel in "{}."'.format(word))
+
+else:
+
+    print('There are {} vowels in "{}."'.format(count, word))
+ 
+
+#print('word "{}" count "{}"'.format(word, count))
+
+#print('There are 0 vowels in "{}."'.format(word))
+
+#if count == 1:
+
+ #print('There is 1 vowel in "{}."'.format(word))
+#elif count == 0:
+#    print('zero')
+#else:
+
+ #print('There are {} vowels in "{}."'.format(count, word))
+
+#print('word {} count {}'.format(word, count))
+
+#print('There is 1 vowel in "{}."'.format(args))
+
+
+#if vowels in char > 1:
+#        count = count + 1
+
+#print('There are {} vowels '.format(count) + 'in "{}."'.format(args))
