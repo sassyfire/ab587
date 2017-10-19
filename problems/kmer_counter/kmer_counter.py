b2b48b713f5d12db96321038a5465900acd7ae28
Pearl Lam <wplam@email.arizona.edu>
2017-10-19 08:58:38 -0700

    working

--- problems/kmer_counter/kmer_counter.py
+++ problems/kmer_counter/kmer_counter.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
+"""Kmer Counter"""
 
-print('OK')
+import sys
+import os
+from collections import Counter
+
+args = sys.argv[1:]
+
+text = ' ' 
+if len(args) == 0:
+    print('Usage: {} LEN STR'.format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
+else:
+    text = args
+
+count = Counter(text.lower())
+
+num = args[0] 
+dna = args[1]
+
+for base in "acgtACGT":
+    if num == int(args[0]):
+        base = sorted(count.keys())
+print('{} {}'.format(num, base))
+
+if num == int(args[0]):
+    print('Kmer size "{}" must be > 0"'.format(num))
+else:
+    print('Kmer size "{}" is not a number"'.format(num))
+
+
+
+    counts.append(str(count[base]))
+print ('{} {} '.format(num, letter))
