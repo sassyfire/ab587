1b33109a4425c91597ece7a4d9c5621b3d74e9af
Pearl Lam <wplam@email.arizona.edu>
2017-10-24 09:34:17 -0700

    done

--- problems/proteins/prot.py
+++ problems/proteins/prot.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
+"""codon table"""
 
-print('OK')
+import sys
+import os
+from collections import Counter
+from collections import defaultdict
+
+args = sys.argv[1:]
+
+if len(args) < 1:
+   print('Usage: {} SEQ'.format(os.path.basename(sys.argv[0])))
+   sys.exit(1)
+
+rna = args[0].upper()
+
+codon_table = dict()
+
+for line in open("codons.rna"):
+    codon, protein = line.rstrip().rsplit()
+    codon_table[codon] = protein    
+
+k = 3 
+proteins = []
+for codon in [rna[i:i+k] for i in range(0, len(rna), k)]:
+     proteins.append(codon_table[codon])
+
+print(''.join(proteins))
+#    print(codon_table[codon], end='')
+
+#print()    
+
