026204ccf5c4461b05759cb6375a003e8f749a71
Pearl Lam <wplam@email.arizona.edu>
2017-12-05 11:14:42 -0700

    done grph

--- problems/grph/grph.py
+++ problems/grph/grph.py
@@ -28,40 +28,13 @@ for record in seq_records:
     seq_pre = str(record.seq[0:3])
     suffixes[seq_suf].append(record.id)
     prefixes[seq_pre].append(record.id)
-    common_keys = set.intersection(*tuple(set(suffixes.keys()) for suffixes in [suffixes, prefixes]))
-    for common_keys in suffixes, prefixes:
-         if len(record.id) > 1:
-             print('{}'.format(record.id))
-         else:
-             print('no')
-#         print('suf {} pre {} common {}'.format(suffixes, prefixes, common_keys))
-#    for record.seq in suffixes:
-#        if  in prefixes:
-#            continue
-#        if :
-#            print('{}'.format(record.id))
-#print('SUFFIX {} PREFIX {}'.format(list(suffixes), list(prefixes)))       
-#print('{}:{}'.format(record.id, (list(set(list(suffixes)).intersection(list(prefixes))))))
-#    for record.seq in suffixes:
-#        if record.seq in prefixes:
-#            print('{} {}'.format(record.seq, record.id))
-#        else:
-#            print('no')
 
-#print record ids of the suffix sequence and prefix sequence
-
-#    for record.seq in sorted(suffixes.items()):
-#        if record.seq in sorted(prefixes.items()):
-#            print('{}'.format(record.id))
-
-#    print('suf {} pre {}'.format(sorted(suffixes.items()), sorted(prefixes.items())))
-
-#print('{} {}'.format(suffixes, prefixes))
-#pairs = list(zip(suffixes.items(), prefixes.items()))
-#pairs = list(zip(suffixes, prefixes))
-#print(' {} {} '.format(suffixes, prefixes)) 
-    
-
+for suffix in suffixes.keys():
+    if suffix in prefixes:
+         for rec_id1 in suffixes[suffix]:
+              for rec_id2 in prefixes[suffix]:
+                  if rec_id1 != rec_id2:
+                      print('{} {}'.format(rec_id1, rec_id2))
 
 #open a file and make sure it is a file read an open file as long as two sequences 
 #are not the same, match the suffix of one sequence to the prefix of another
